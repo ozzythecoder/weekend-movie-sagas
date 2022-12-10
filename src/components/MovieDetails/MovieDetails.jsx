a78dc91a76f93c849c099f36eeabeb57e647d23d
August McAllister <august.chm@gmail.com>
2022-12-10 17:47:34 -0600

    MovieDetails is getting individual movies from the store by id

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux";
 
 export default function MovieDetails() {
 
   const { id } = useParams();
 
+  const movie = useSelector(store => store.movies.filter(e => e.id == id)[0])
+
+  console.log(movie);
+
   return (
-    <div>Hey {id}</div>
+    <div>Hey {movie.title}</div>
   )
 }
