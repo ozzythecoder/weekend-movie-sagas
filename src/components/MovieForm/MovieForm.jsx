c697a9f66960ce5dfe349e50b05b8fb0aa2e7f2b
August McAllister <august.chm@gmail.com>
2022-12-11 11:04:19 -0600

    Reactive styling of add movie form

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,22 +1,48 @@
-
-
 export default function MovieForm() {
-
   return (
     <div>
       <form className="page-form">
-        <input />
-        <input />
-        <textarea>
+        
+        <label className="form-label" labelfor="movie-title-in">
+          Movie Title:
+        </label>
+        <input
+          className="form-single-input"
+          id="movie-title-in"
+          placeholder="Title" />
 
-        </textarea>
-        <select>
-
+        <label className="form-label" labelfor="movie-poster-in">
+          Movie Poster URL:
+        </label>
+        <input
+          type="text"
+          id="movie-poster-in"
+          className="form-single-input"
+          placeholder="Image URL"
+          />
+        
+        <label
+          className="form-label"
+          labelfor="movie-description-in" >
+          Description:
+        </label>
+        <textarea className="form-text-input"
+          placeholder="Type a brief description"></textarea>
+        
+        <label className="form-label" labelfor="movie-genre-in">
+          Genre:
+        </label>
+        <select className="form-select">
+          <option>Test</option>
+          <option>Test 2</option>
         </select>
-        <button>
+        
+        <div></div>
+        
+        <button className="" type="submit">
           Submit
         </button>
       </form>
     </div>
-  )
+  );
 }
