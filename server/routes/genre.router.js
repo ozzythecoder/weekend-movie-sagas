1e80278093cb2aae229593e95b39127047e225cf
August McAllister <august.chm@gmail.com>
2022-12-11 11:07:09 -0600

    Added DB query for genres

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+
+  pool.query('SELECT * FROM genres')
+    .then(dbRes => {
+      res.send(dbRes.rows)
+    })
+    .catch(err => {
+      console.log('genres router', err)
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
